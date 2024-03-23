d6e00cfe164b71cdaf88cc2d69e8de022836961f
sumikawadenko <sumikawadenko@gmail.com>
2024-03-24 01:58:45 +0900

    時間差分dtをリアルタイムで更新するように変更

--- hex_cylinder_raound/hex_cylinder_raound.ino
+++ hex_cylinder_raound/hex_cylinder_raound.ino
@@ -2,55 +2,65 @@
 
 const int numCylinders = 1;
 Servo servos[numCylinders];
-int val1 = A1; // シリンダー
-int ls[numCylinders]; // リニアセンサーの値
-float targetAngle[numCylinders]; // 目標角度
-float currentAngle[numCylinders]; // 現在の角度
-float error[numCylinders]; // 現在の偏差
-float prevError[numCylinders] = {0.0}; // 前回の偏差
-float integral[numCylinders]; // 積分項
-float derivative[numCylinders]; // 微分項
-float kp = 0.8; // 比例ゲイン
-float ki = 1.2; // 積分ゲイン
-float kd = 0.5; // 微分ゲイン
-float dt = 0.001; // 固定の周期（例: 0.01秒）
-float integralMin = -100.0; // 積分項の下限値
-float integralMax = 100.0; // 積分項の上限値
-float outputOld=0.0;
+int val1 = A1;                            // シリンダー
+int ls[numCylinders];                     // リニアセンサーの値
+float targetAngle[numCylinders];          // 目標角度
+float currentAngle[numCylinders];         // 現在の角度
+float error[numCylinders];                // 現在の偏差
+float prevError[numCylinders] = { 0.0 };  // 前回の偏差
+float integral[numCylinders];             // 積分項
+float derivative[numCylinders];           // 微分項
+float kp = 0.8;                           // 比例ゲイン
+float ki = 1.2;                           // 積分ゲイン
+float kd = 0.5;                           // 微分ゲイン
+float dt = 0;                             // 前回との時間差分
+uint32_t nowTime = 0;                     //今の時間(ms)
+uint32_t oldTime = 0;                     //前回時間(ms)
+float integralMin = -100.0;               // 積分項の下限値
+float integralMax = 100.0;                // 積分項の上限値
+float outputOld = 0.0;
 
-char direction[numCylinders] = {1}; // ポテンショメーターの値を模擬的に変化させる方向を保持する変数
-int simulatedPotValue = 0; // ポテンショメーターの値を模擬的に保持する変数
+char direction[numCylinders] = { 1 };  // ポテンショメーターの値を模擬的に変化させる方向を保持する変数
+int simulatedPotValue = 0;             // ポテンショメーターの値を模擬的に保持する変数
 
 void setup() {
   Serial.begin(9600);
 
-  // 各シリンダーのサーボをピンにアタッチ
-  // servos[0].attach(2, 500, 2400);
-  // servos[1].attach(3, 500, 2400);
-  // servos[2].attach(4, 500, 2400);
-  // servos[3].attach(5, 500, 2400);
-  // servos[4].attach(6, 500, 2400);
-  // servos[5].attach(7, 500, 2400);
-
-  for (int i=0; i<numCylinders; i++){
-    servos[i].attach(i+2, 500, 2400);
+  //サーボのピン番号を指定
+  for (int i = 0; i < numCylinders; i++) {
+    /*---
+    各モータにおけるパルス幅
+    SG90 : 500-2400(us)
+    MG90 : 1000-2000(us)
+    ---*/
+    servos[i].attach(i + 2, 1000, 2000);
   }
+  //oldTime初期化
+  oldTime = micros();
 }
 
 void loop() {
+  //現在時刻取得(ms)
+  nowTime = micros();
+  //時間差分計算
+  dt = (nowTime - oldTime);
+  dt = float(dt) / 1000.0;
+  //oldTime初期化
+  oldTime = nowTime;
+
   // ポテンショメーターの値を模擬的に変化させる
   simulatedPotValue += 3 * direction[0];
   if (simulatedPotValue >= 1023 || simulatedPotValue <= 0) {
-    direction[0] *= -1; // 値が 0 から 1023 の範囲を超えたら方向を反転させる
+    direction[0] *= -1;  // 値が 0 から 1023 の範囲を超えたら方向を反転させる
   }
 
   // 各シリンダーごとの処理
   for (int i = 0; i < numCylinders; i++) {
-    ls[i] = analogRead(val1); // リニアセンサーの値を読み取る
+    ls[i] = analogRead(val1);  // リニアセンサーの値を読み取る
 
     // エアシリンダーの角度を計算
-    targetAngle[i] = map(simulatedPotValue, 0, 1023, 20, 170); // ポテンショメーターの範囲を角度の範囲にマッピング
-    currentAngle[i] = map(ls[i], 1023, 400, 20, 170); // リニアセンサーの範囲を角度の範囲にマッピング
+    targetAngle[i] = map(simulatedPotValue, 0, 1023, 20, 170);  // ポテンショメーターの範囲を角度の範囲にマッピング
+    currentAngle[i] = map(ls[i], 1023, 0, 20, 170);             // リニアセンサーの範囲を角度の範囲にマッピング
 
     // 偏差を計算
     error[i] = targetAngle[i] - currentAngle[i];
@@ -64,7 +74,7 @@ void loop() {
     //   integral[i] = integralMax;
     // }
     derivative[i] = error[i] - prevError[i];
-    float output = kp * error[i] + ki * integral[i] + kd * derivative[i]/dt;
+    float output = kp * error[i] + ki * integral[i] + kd * derivative[i] / dt;
     // if ((outputOld-output)>1){
     //   output=output+1;
     // }else if((outputOld-output)<-1){
@@ -72,12 +82,13 @@ void loop() {
     // }
     // outputOld=output;
 
-    // 制御信号をサーボモーターに出力
-    if(output>90){
-      output=90;
-    }else if(output<-90){
-      output=-90;
+    //outputがサーボの範囲を超えすぎないように制限
+    if (output > 90) {
+      output = 90;
+    } else if (output < -90) {
+      output = -90;
     }
+    // 制御信号をサーボモーターに出力
     servos[i].write(90.0 + output);
 
     // シリアルモニタに出力
@@ -92,10 +103,11 @@ void loop() {
     Serial.print(", ");
     Serial.print("output:");
     Serial.print(output);
+    Serial.print(", ");
+    Serial.print("dt:");
+    Serial.print(dt);
   }
   Serial.println();
-
-  // delay(10);
 
   // 前回の偏差を更新
   for (int i = 0; i < numCylinders; i++) {
