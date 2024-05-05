607c5fb4ed5f36ac3a84bcbfc2e5d5e273194637
sumikawadenko <sumikawadenko@gmail.com>
2024-05-05 22:09:07 +0900

    速度型PIDひな形

--- hex_cylinder_raound/hex_cylinder_raound.ino
+++ hex_cylinder_raound/hex_cylinder_raound.ino
@@ -1,41 +1,54 @@
 #include <Servo.h>
 #include <math.h>
 
-#define PI 3.1415
-#define targetMode 2
+#define kp 0.1    // 比例ゲイン
+#define ki 0.01   // 積分ゲイン
+#define kd 0.008  // 微分ゲイン
+
 /*---
-ターゲット位置の波形
+目標値の波形
 0 : 三角波
 1 : 矩形波
 2 : 正弦波
 ---*/
+#define targetMode 2
+//目標値の波形の周波数(Hz)
 #define targetFreq 0.5
-//ターゲット位置の波形の周波数(Hz)
+//シリンダーの個数
+#define numCylinders 1
 
-const int numCylinders = 1;
+#define PI 3.1415
+
 Servo servos[numCylinders];
-int val1 = A1;                            // シリンダー
-int ls[numCylinders];                     // リニアセンサーの値
-float targetAngle[numCylinders];          // 目標角度
-float currentAngle[numCylinders];         // 現在の角度
-float error[numCylinders];                // 現在の偏差
-float prevError[numCylinders] = { 0.0 };  // 前回の偏差
-float integral[numCylinders];             // 積分項
-float derivative[numCylinders];           // 微分項
-float kp = 0.8;                           // 比例ゲイン
-float ki = 1.2;                           // 積分ゲイン
-float kd = 0.5;                           // 微分ゲイン
-float dt = 0;                             // 前回との時間差分
-float dtTgt = 0;                          //Target位置生成用の時間差分
-uint32_t nowTime = 0;                     //今の時間(ms)
-uint32_t oldTime = 0;                     //前回時間(ms)
-float integralMin = -100.0;               // 積分項の下限値
-float integralMax = 100.0;                // 積分項の上限値
-float outputOld = 0.0;
 
-char direction = 1;         // ポテンショメーターの値を模擬的に変化させる方向を保持する変数
-int simulatedPotValue = 0;  // ポテンショメーターの値を模擬的に保持する変数
+uint8_t posSenPinNo[numCylinders] = { A1 };  // ポジションセンサーの繋がっているポート番号
+uint8_t valvePinNo[numCylinders] = { 3 };    // バルブの繋がっているポート番号
+int16_t valGetPosSen[numCylinders];          // ポジションセンサーの取得値
+int16_t targetPos[numCylinders];             // 目標位置
+int16_t currentPos[numCylinders];            // 現在位置
 
+float err[numCylinders];         // 現在の偏差
+float prevErr[numCylinders];     // 前回の偏差
+float errVel[numCylinders];      // 現在の偏差速度
+float prevErrVel[numCylinders];  // 前回の偏差速度
+float errAcc[numCylinders];      // 現在の偏差加速度
+
+float dt = 0;          // 前回との時間差分
+float dtTgt = 0;       //Target位置生成用の時間差分
+uint32_t nowTime = 0;  //今の時間(ms)
+uint32_t oldTime = 0;  //前回時間(ms)
+
+float dOutput = 0.0;  //出力変化量
+float output = 0.0;   //出力(バルブ開度)
+
+uint8_t direction = 1;           // 目標値を変化させる方向を保持する変数
+uint16_t simulatedPotValue = 0;  // 目標値を保持する変数
+
+uint8_t i = 0;
+uint8_t j = 0;
+char buf[255];
+
+//目標値生成用の関数を宣言
 void targetGen(void);
 
 void setup() {
@@ -48,7 +61,7 @@ void setup() {
     SG90 : 500-2400(us)
     MG90 : 1000-2000(us)
     ---*/
-    servos[i].attach(i + 2, 1000, 2000);
+    servos[i].attach(valvePinNo[i], 1000, 2000);
   }
   //oldTime初期化
   oldTime = micros();
@@ -66,32 +79,31 @@ void loop() {
   targetGen();
 
   // 各シリンダーごとの処理
-  for (int i = 0; i < numCylinders; i++) {
-    ls[i] = analogRead(val1);  // リニアセンサーの値を読み取る
+  for (i = 0; i < numCylinders; i++) {
+    valGetPosSen[i] = analogRead(posSenPinNo[i]);  // ポジションセンサーの値を読み取る
 
     // エアシリンダーの角度を計算
-    targetAngle[i] = map(simulatedPotValue, 0, 1023, 20, 170);  // ポテンショメーターの範囲を角度の範囲にマッピング
-    currentAngle[i] = map(ls[i], 1023, 0, 20, 170);             // リニアセンサーの範囲を角度の範囲にマッピング
+    targetPos[i] = map(simulatedPotValue, 0, 1023, 0, 1023);  //目標値を10bit値に変換
+    currentPos[i] = map(valGetPosSen[i], 1023, 0, 0, 1023);   //ポジションセンサーの値を10bit値に変換
 
     // 偏差を計算
-    error[i] = targetAngle[i] - currentAngle[i];
+    err[i] = float(targetPos[i] - currentPos[i]);
 
-    // PID制御計算
-    integral[i] += error[i] * dt;
-    // 積分項の制限
-    // if (integral[i] < integralMin) {
-    //   integral[i] = integralMin;
-    // } else if (integral[i] > integralMax) {
-    //   integral[i] = integralMax;
+    // 偏差速度を計算
+    errVel[i] = (err[i] - prevErr[i]) / dt;
+
+    // 偏差加速度を計算
+    errAcc[i] = (errVel[i] - prevErrVel[i]) / dt;
+
+    //出力変化量を計算
+    dOutput = kp * err[i] + ki * errVel[i] + kd * errAcc[i];
+
+    // if (err[i] != prevErr[i]) {
+    //   output = 0;
     // }
-    derivative[i] = error[i] - prevError[i];
-    float output = kp * error[i] + ki * integral[i] + kd * derivative[i] / dt;
-    // if ((outputOld-output)>1){
-    //   output=output+1;
-    // }else if((outputOld-output)<-1){
-    //   output=output-1;
-    // }
-    // outputOld=output;
+
+    //出力を計算
+    output += dOutput;
 
     //outputがサーボの範囲を超えすぎないように制限
     if (output > 90) {
@@ -99,33 +111,32 @@ void loop() {
     } else if (output < -90) {
       output = -90;
     }
+
     // 制御信号をサーボモーターに出力
     servos[i].write(90.0 + output);
 
+    // 前回の偏差を更新
+    prevErr[i] = err[i];
+    prevErrVel[i] = errVel[i];
+
     // シリアルモニタに出力
-    Serial.print("targetAngle:");
-    Serial.print(targetAngle[i]);
+    sprintf(buf, "targetPos%01d:", (int)i);
+    Serial.print(buf);
+    Serial.print(targetPos[i]);
     Serial.print(", ");
-    Serial.print("currentAngle:");
-    Serial.print(currentAngle[i]);
-    Serial.print(", ");
-    Serial.print("i:");
-    Serial.print(i + 1);
+    sprintf(buf, "currentPos%01d:", (int)i);
+    Serial.print(buf);
+    Serial.print(currentPos[i]);
     Serial.print(", ");
     Serial.print("output:");
-    Serial.print(output);
+    Serial.print(output * 2);
     // Serial.print(", ");
     // Serial.print("dt:");
     // Serial.print(dt);
     // Serial.print(", ");
     // Serial.print("nowTime:");
     // Serial.print(float(nowTime / 1000000.0));
-  }
-  Serial.println();
-
-  // 前回の偏差を更新
-  for (int i = 0; i < numCylinders; i++) {
-    prevError[i] = error[i];
+    Serial.println();
   }
 }
 
