1ac630154c15be1f319b0ec1ca35046180da1ffc
sumikawadenko <sumikawadenko@gmail.com>
2024-09-19 23:46:55 +0900

    バルブコマンドの送信内容を統一

--- main/main.ino
+++ main/main.ino
@@ -117,9 +117,9 @@ void loop() {
       case 63:
         for (int i = 0; i < valveTotalNum; i++) {  //バルブの個数だけ繰り返す
 
-          if (dataAry[i] == 1) {                  //もしシリンダーiのデータがcommand指示なら
-            commandFlagAry[i] = 1;                //コマンドフラグをcommandにする
-            commandAry[i] = dataAry[i + 4] / 10;  //コマンドを送られてきたデータに設定(0-1800で受信するため10で割る)
+          if (dataAry[i] == 1) {                       //もしシリンダーiのデータがcommand指示なら
+            commandFlagAry[i] = 1;                     //コマンドフラグをcommandにする
+            commandAry[i] = dataAry[i + 4] / 10 - 90;  //コマンドを送られてきたデータに設定(0-1800で受信するため10で割る)
 
           } else if (dataAry[i] == 0) {     //もしシリンダーiのデータがposition指示なら
             commandFlagAry[i] = 0;          //コマンドフラグをpositionにする
@@ -214,7 +214,7 @@ void loop() {
     posAry[i][1] = buf;  //Mappingされた値を反映
 
     if (commandFlagAry[i]) {                                                //もしコマンドフラグがcommandなら
-      valve[i].write(commandAry[i]);                                        //バルブへcommandの値をそのまま送信
+      valve[i].write(commandAry[i] + 90);                                   //バルブへcommandの値をそのまま送信
       vCommand[i].timeReset();                                              //念のためPID処理の時間をリセットしておく
     } else {                                                                //もしコマンドフラグがpositionなら
       commandAry[i] = vCommand[i].calcCommand(posAry[i][0], posAry[i][1]);  //PID関数を実行しcommandを取得
